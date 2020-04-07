b78fa4ea618a6c3ca86d35e3f5ba04465c6aaf19
diditiswantoro123 <55912689+diditiswantoro123@users.noreply.github.com>
2020-04-07 08:34:17 +0700

    Add files via upload

---
+++ simplerestandroid-master/simplerestandroid-master/web_service/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id11924761_cordova');
+ define('PASS','Z\@!Y2PK[eth00pZ');
+ define('DB','id11924761_app');
+
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
